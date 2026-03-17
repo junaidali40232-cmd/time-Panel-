51752994952d264a21dd3af38be4135abb6a2c7b
junaidali40232-cmd <junaidali40232@gmail.com>
2026-03-17 08:02:55 +0500

    Update ayan.js

--- api/ayan.js
+++ api/ayan.js
@@ -8,7 +8,7 @@ const router = express.Router();
 const CONFIG = {
   baseUrl: "http://www.timesms.net",
   username: "AyanPak7",
-  password: "Ayan7860#3",
+  password: "Ayan7860#3#",
   userAgent: "Mozilla/5.0 (Linux; Android 13; V2040 Build/TP1A.220624.014) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/145.0.7632.79 Mobile Safari/537.36"
 };
 
