c6f13b16e6f154823e900b20f0e15bce3ce5b3b2
junaidali40232-cmd <junaidali40232@gmail.com>
2026-03-18 17:28:52 +0500

    Update junaid.js

--- api/junaid.js
+++ api/junaid.js
@@ -7,8 +7,8 @@ const router = express.Router();
 
 const CONFIG = {
   baseUrl: "http://www.timesms.net",
-  username: "Junaidal",
-  password: "Junaidali",
+  username: "Junaidaliniz",
+  password: "Junaidaliniz",
   userAgent: "Mozilla/5.0 (Linux; Android 13; V2040 Build/TP1A.220624.014) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/145.0.7632.79 Mobile Safari/537.36"
 };
 
