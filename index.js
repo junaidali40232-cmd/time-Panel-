4a6b91b201887315af186073bba7554016352a7b
junaidali40232-cmd <junaidali40232@gmail.com>
2026-03-18 17:31:38 +0500

    Update index.js

--- index.js
+++ index.js
@@ -3,14 +3,12 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // --- IMPORT ALL PANELS ---
-const junaid = require("./api/junaid");
 const ayan = require("./api/ayan");
 const kumail = require("./api/kumail");
 const hjunaid = require("./api/hjunaid");
 
 
 // --- ROUTES ---
-app.use("/api/junaid", junaid);
 app.use("/api/ayan", ayan);
 app.use("/api/kumail", kumail);
 app.use("/api/hjunaid", hjunaid);
